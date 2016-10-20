d9379e0de427499601f57bcbf92466459ee004a8
d0p1 <d0p1@yahoo.com>
2016-10-20 17:10:22 +0200

    todo

--- pfc/lexer.py
+++ pfc/lexer.py
@@ -2,9 +2,12 @@ from pfc import io
 
 class Lexer:
 
+	WHITESPACE = [' ', '\t']
+
 	def __init__(self, line_length):
 		self.line_length = line_length
 		self.errors = 0
+		self.tokens = dict()
 
 	def __lineCount(self, stream):
 		for index, line in enumerate(stream):
@@ -16,12 +19,20 @@ class Lexer:
 			return True
 		return False
 
+	def pushToken(self, token, lineno):
+		if not lineno in self.tokens:
+			self.tokens[lineno] = list()
+		self.tokens[lineno].apppend(token)
+
 	def lex(self, stream):
 		for index, line in self.__lineCount(stream):
 			if len(line) <= self.line_length:
 				if self.__isComment(line):
 					continue
-				print("not a comment")
+				if line[0] not in Lexer.WHITESPACE:
+					io.error(index,
+						"colum 0 must be a whitespace")
+					self.errors += 1
 			else:
 				io.error(index, "Line too long: {} > {}".format(
 					len(line), self.line_length))
