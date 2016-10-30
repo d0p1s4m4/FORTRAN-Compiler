13f77451f299825a732946400f83a5cf9770dacc
d0p1 <d0p1@yahoo.com>
2016-10-30 22:51:58 +0100

    check 1-5 column yeah I'm busy so I can't work too much :<

--- pfc/lexer.py
+++ pfc/lexer.py
@@ -1,3 +1,4 @@
+import re
 from pfc import io
 
 class Lexer:
@@ -24,22 +25,26 @@ class Lexer:
 			self.errors += 1
 		return False
 
-	def __checkLineStart(self, line, lineno):
-		return True
+	def __checkStatementNumber(self, line, lineno):
+		result = re.match(r"^(?:\s+)?([0-9]+)?(?:\s+)?$", line)
+		if not result:
+			io.error(lineno, "Column 1-5 illegal data")
+			self.errors += 1
+			return
+		if result.group(1):
+			self.pushToken(("LINE_LABEL", result.group(1)), lineno)
 
 	def pushToken(self, token, lineno):
 		if not lineno in self.tokens:
 			self.tokens[lineno] = list()
-		self.tokens[lineno].apppend(token)
+		self.tokens[lineno].append(token)
 
 	def lex(self, stream):
 		for index, line in self.__lineCount(stream):
 			if len(line) <= self.line_length:
 				if self.__isComment(line, index):
 					continue
-				print(line)
-				if not self.__checkLineStart(line, lineno):
-					io.error(index, "Malformated line")
+				self.__checkStatementNumber(line[1:5], index)
 			else:
 				io.error(index, "Line too long: {} > {}".format(
 					len(line), self.line_length))
